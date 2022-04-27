c72b23a0d10c9cb664ac5e09b0812d776ed148ed
olavsl <olav.selnes.lorentzen@hotmail.com>
2022-04-27 22:15:47 +0200

    Removed unnecessary timer.

--- src/main/java/iqbattles/CountDownTimer.java
+++ src/main/java/iqbattles/CountDownTimer.java
@@ -1,14 +1,11 @@
 package iqbattles;
 
-import java.util.Timer;
 import java.util.TimerTask;
 
 public class CountDownTimer extends TimerTask {
 
     private int initialTime;
     private int time;
-
-    private static Timer timer;
 
     public CountDownTimer(int time) {
         this.initialTime = time;
@@ -18,10 +15,6 @@ public class CountDownTimer extends TimerTask {
     @Override
     public void run() {
         this.time--;
-
-        if (this.time == 0) {
-            timer.cancel();
-        }
     }
 
     public void reset() {
