50c0404fa33ec11e19e5d8bd58ea4901687aee36
olavsl <olav.selnes.lorentzen@hotmail.com>
2022-04-27 22:36:38 +0200

    Finished test.

--- src/test/java/iqbattles/LogInVerifierTest.java
+++ src/test/java/iqbattles/LogInVerifierTest.java
@@ -1,10 +1,43 @@
 package iqbattles;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LogInVerifierTest {
+    
     @Test
+    @DisplayName("Test log in function")
     void testLogIn() {
-        
+        LogInVerifier logInVerifier = new LogInVerifier();
+        Filehandler filehandler = new Filehandler();
+
+        String username = "abcd";
+        String password = "abcd";
+
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+            String hashedPassword = ToHexString.toHexString(md.digest(password.getBytes(StandardCharsets.UTF_8)));
+
+            Player player = new Player(username, hashedPassword, 100, 0, 0);
+
+            String playerInfo = player.getUsername() + " " + player.getHashedPassword() + " " + player.getRating() + " 0 0";
+
+            ArrayList<String> input = new ArrayList<>(Arrays.asList(playerInfo));
+            filehandler.writeToFile("playersTest.txt", input);
+
+            assertEquals(player.toString(), logInVerifier.LogIn(username, password, "playersTest.txt").toString());
+
+            filehandler.clearFile("playersTest.txt");
+        } catch (Exception e) {}
     }
 }
